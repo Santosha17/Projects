f186f8177f4ee17b5818b69a2ae2ec180b54d50f
Jonny <jpsantosrm@gmail.com>
2023-01-20 08:58:17 +0000

    Updated

--- Project_Final.sh
+++ Project_Final.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 #Get prefix from user
-read -p "Insira or prefix: " prefix
+read -p "Insira o prefix: " prefix
 
 #Get the folder path
 read -p "Insira a pasta: " folder
+
+#Go to the specific folder
+cd "$folder"
+
+# Loop through all files in the folder
+for file in *
+do
+    # Get the file name and extension
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    # Add the prefix to the file name
+    new_filename="$prefix$filename.$extension"
+
+    # Rename the file
+    mv "$file" "$new_filename"
+done
+
+echo "All files in the folder have been renamed with the prefix $prefix"
