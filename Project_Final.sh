f98bb706c86ff9fc964de1830b5a3ea955073d40
Jonny <jpsantosrm@gmail.com>
2023-01-25 18:32:59 +0000

    update

--- Project_Final.sh
+++ Project_Final.sh
@@ -6,57 +6,57 @@ echo "2 - 3 a 6 meses"
 echo "3 - 6 meses a 1 ano"
 echo "4 - 1 anos a 3 anos"
 echo "5 - Mais de 3 anos"
-read pergunta_a
+read -r pergunta_a
 echo "B. Como avalias o teu serviço?" 
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_b
+read -r pergunta_b
 echo "C. Como avalias a tua resposta ao serviço?" 
 echo "1 - Custa me devolver o serviço, principalmente os serviços rápidos e ao vidro"
 echo "2 - Respondo a bolas lentas e simples apesar de os serviços com mais potência tenho dificuldade a a responder, deixando bolas fáceis para os rivais"
 echo "3 - Consigo responder com segurança aos serviços não muito complicados"
 echo "4 - Respondo com bastante segurança a todos os serviços com alguma potência"
 echo "5 - Tenho uma resposta fiável e com direção"
-read pergunta_c
+read -r pergunta_c
 echo "D. Como avalias o teu volley?" 
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_d
+read -r pergunta_d
 echo "E. Como avalias os teus ressaltos?" 
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_e
+read -r pergunta_e
 echo "F. Como avalias o teu lob?" 
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_f
+read -r pergunta_f
 echo "G. Como avalias a tua pancada de esquerda?"
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_g
+read -r pergunta_g
 echo "H. Como avalias a tua pancada de direita?"
 echo "1 - Péssimo"
 echo "2 - Mau"
 echo "3 - Bom"
 echo "4 - Muito Bom"
 echo "5 - Excelente"
-read pergunta_h
-total=$(($pergunta_a + $pergunta_b + $pergunta_c + $pergunta_d +$pergunta_e + $pergunta_f + $pergunta_g + $pergunta_h +))
+read -r pergunta_h
+total=$(( pergunta_a + pergunta_b + pergunta_c + pergunta_d + pergunta_e + pergunta_f + pergunta_g + pergunta_h ))
 echo "$total"
 if [[ $total -lt 10 ]]; then
      echo "M5"
