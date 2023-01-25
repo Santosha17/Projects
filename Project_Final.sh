86c01984821d75f3354d680b11814681c16648c1
Jonny <jpsantosrm@gmail.com>
2023-01-25 17:30:04 +0000

    new project

--- Project_Final.sh
+++ Project_Final.sh
@@ -1,27 +1,10 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-#Get prefix from user
-read -p "Insira o prefix: " prefix
-
-#Get the folder path
-read -p "Insira a pasta: " folder
-
-#Go to the specific folder
-cd "$folder"
-
-# Loop through all files in the folder
-for file in *
-do
-    # Get the file name and extension
-    filename=$(basename "$file")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-
-    # Add the prefix to the file name
-    new_filename="$prefix$filename.$extension"
-
-    # Rename the file
-    mv "$file" "$new_filename"
-done
-
-echo "All files in the folder have been renamed with the prefix $prefix"
+echo "1. Há quanto tempo jogas Padel?" 
+echo "2. Como avalias o teu serviço?" 
+echo "3. Como avalias a tua resposta ao serviço?" 
+echo "4. Como avalias o teu volley?" 
+echo "5. Como avalias os teus ressaltos?" 
+echo "6. Como avalias o teu lob?" 
+echo "7. Como avalias a tua pancada de esquerda?"
+echo "8. Como avalias a tua pancada de direita?"
