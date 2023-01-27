2234507486af692a6ad65ee7d0b9756037f08db5
Jonny <jpsantosrm@gmail.com>
2023-01-27 16:34:33 +0000

    New Project and Readme updated

---
+++ Project.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+# Mostra informações do sistema operacional
+echo ""
+echo "<--------------System Info------------>"
+echo "Sistema operacional: $(uname -r)"
+echo "Nome de utilizador: $(uname -n)"
+arquitetura=$(uname -i)
+echo Arquitetura: "$arquitetura"
+if [[ $arquitetura == x86_64* ]]; then
+    echo "Arquitetura x64"
+elif [[ $arquitetura == i486* ]]; then
+    echo "Arquitetura x32"
+elif  [[ $arquitetura == ^arm* ]]; then
+    echo "Arquitetura ARM"
+fi
+echo ""
+echo "<--------------CPU Info--------------->"
+# Mostra informações do processador (CPU)
+cpu_model=$(grep "model name" /proc/cpuinfo | head -n 1 | awk -F ':' '{print $2}')
+cpu_cores=$(grep -c "processor" /proc/cpuinfo)
+echo "Processador: $cpu_model"
+echo "Número de núcleos: $cpu_cores"
+echo "Número de threads: $cpu_threads"
+echo ""
+echo "<--------------Memory Info------------>"
+# Mostra informações da memória RAM
+mem_total=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+mem_free=$(awk '/MemFree/ {print $2}' /proc/meminfo)
+echo "Memória total: $(($mem_total / 1024))MB"
+echo "Memória livre: $(($mem_free / 1024))MB"
+echo ""
+echo "<--------------Disc Info-------------->"
+# Mostra informações do disco 
+disk_total=$(df -h / | awk '{print $3}' | tail -n 1)
+disk_used=$(df -h / | awk '{print $4}' | tail -n 1)
+disk_free=$(df -h / | awk '{print $5}' | tail -n 1)
+echo "Espaço total do disco: $disk_total"
+echo "Espaço utilizado do disco: $disk_used"
+echo "Espaço livre no disco: $disk_free"
+echo ""
+
+
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Projects
 Projeto de Scipting
 
-Tema: Inquérito autónome sobre a técnica de padel
+Tema: Inquérito autónomo sobre a técnica de padel
+Tema 2: Script que mostra o status do computador (CPU, RAM, DISCO)
 
 Autor: João Santos
