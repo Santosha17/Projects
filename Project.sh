74cbc260581b03f9e1b81d09df3546d8ea4ad7bd
Jonny <jpsantosrm@gmail.com>
2023-01-30 15:32:21 +0000

    updates

--- Project.sh
+++ Project.sh
@@ -3,22 +3,21 @@
 # Mostra informações do sistema operacional
 echo ""
 echo "<--------------System Info------------>"
-echo "Sistema operacional: $(uname -r)"
-echo "Nome de utilizador: $(uname -n)"
-arquitetura=$(uname -i)
-echo Arquitetura: "$arquitetura"
+echo "Sistema operacional: $(uname -a)" #Verifica a versão do sistema operativo
+echo "Nome de utilizador: $(uname -n)" #Verifica o nome de utilizador
+arquitetura=$(uname -m) #Identifica a versão do kernel
 if [[ $arquitetura == x86_64* ]]; then
-    echo "Arquitetura x64"
+    echo "Arquitetura: x64"
 elif [[ $arquitetura == i486* ]]; then
-    echo "Arquitetura x32"
+    echo "Arquitetura: x32"
 elif  [[ $arquitetura == ^arm* ]]; then
-    echo "Arquitetura ARM"
+    echo "Arquitetura: ARM"
 fi
 echo ""
 echo "<--------------CPU Info--------------->"
 # Mostra informações do processador (CPU)
-cpu_model=$(grep "model name" /proc/cpuinfo | head -n 1 | awk -F ':' '{print $2}')
-cpu_cores=$(grep -c "processor" /proc/cpuinfo)
+cpu_model=$(grep "model name" /proc/cpuinfo | head -n 1 | awk -F ':' '{print $2}') #Mostra a info do CPU
+cpu_cores=$(grep -c "processor" /proc/cpuinfo) #Mostra o númerto de núcleos
 #cpu_threads=$(lscpu | grep "Thread(s) per core" | awk '{print $2}')
 echo "Processador: $cpu_model"
 echo "Número de núcleos: $cpu_cores"
@@ -29,8 +28,8 @@ echo "<--------------Memory Info------------>"
 mem_total=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
 mem_free=$(awk '/MemFree/ {print $2}' /proc/meminfo)
 #mem_used=$(awk '/MemFree/ {print $2}' /proc/meminfo)
-echo "Memória total: $(($mem_total / 1024))MB"
-echo "Memória livre: $(($mem_free / 1024))MB"
+echo "Memória total: $((mem_total / 1024))MB"
+echo "Memória livre: $((mem_free / 1024))MB"
 #echo "Mémoria usada: $(($mem_used / 1024))MB"
 echo ""
 echo "<--------------Disc Info-------------->"
@@ -39,8 +38,8 @@ disk_total=$(df -h / | awk '{print $3}' | tail -n 1)
 disk_used=$(df -h / | awk '{print $4}' | tail -n 1)
 disk_free=$(df -h / | awk '{print $5}' | tail -n 1)
 echo "Espaço total do disco: $disk_total"
-echo "Espaço utilizado do disco: $disk_used"
-echo "Espaço livre no disco: $disk_free"
+echo "Espaço utilizado do disco: $disk_used"
+echo "Espaço livre no disco: $disk_free"
 echo ""
 
 
--- Project_Final.sh
+++ Project_Final.sh
@@ -25,14 +25,20 @@ for pergunta in $(cat $filename); do # Lê linha a linha a partir do ficheiro .c
 done
 
 echo "Total: $total"
-if [[ $total -lt 15 ]]; then # Menos de 15 M5
-     echo "Categoria: M5"
-elif [[ $total -lt 25 ]]; then # Menos de 25 M4
-     echo "Categoria: M4"
-elif [[ $total -lt 35  ]]; then # Menos de 35 M3
-     echo "Categoria: M3"
-elif [[ $total -lt 45  ]]; then # Menos de 45 M2
-     echo "Categoria: M2"
-else
-     echo "Categoria: M1" # Entre 45 e 50 M1
+
+if [[ $2 = "padel" ]]; then #Padel no fim do comando
+     if [[ $total -lt 15 ]]; then # Menos de 15 M5
+          echo "Categoria: M5"
+     elif [[ $total -lt 25 ]]; then # Menos de 25 M4
+          echo "Categoria: M4"
+     elif [[ $total -lt 35  ]]; then # Menos de 35 M3
+          echo "Categoria: M3"
+     elif [[ $total -lt 45  ]]; then # Menos de 45 M2
+          echo "Categoria: M2"
+          else
+          echo "Categoria: M1" # Entre 45 e 50 M1
+     fi   
 fi
+
+# Este if faz com que se não tiver padel no fim do comando ex:"bash Project_Final.sh perguntas.csv", ele faz só a soma e não inclui o ranking de padel
+# Se tiver padel no fim ex:"bash Project_Final.sh perguntas.csv padel", no fim diz a categoria de acordo com o total
